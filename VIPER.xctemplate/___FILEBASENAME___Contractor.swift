6f1e6905161fed6b9f769df543636af189d24321
Pedro Esli <pedroesli@users.noreply.github.com>
2023-06-12 19:47:28 -0300

    Update ___FILEBASENAME___Contractor.swift

--- VIPER.xctemplate/___FILEBASENAME___Contractor.swift
+++ VIPER.xctemplate/___FILEBASENAME___Contractor.swift
@@ -4,19 +4,19 @@ import UIKit
 
 // MARK: View
 protocol ___VARIABLE_productName___ViewProtocol: AnyObject {
-    var presenter: ___VARIABLE_productName___PresenterProtocol? {get set}
+    var presenter: ___VARIABLE_productName___PresenterProtocol? { get set }
 }
 
 // MARK: Interactor
 protocol ___VARIABLE_productName___InteractorProtocol: AnyObject {
-    var presenter: ___VARIABLE_productName___PresenterProtocol? {get set}
+    var presenter: ___VARIABLE_productName___PresenterProtocol? { get set }
 }
 
 // MARK: Presenter
 protocol ___VARIABLE_productName___PresenterProtocol: AnyObject {
-    var router: ___VARIABLE_productName___RouterProtocol? {get set}
-    var interactor: ___VARIABLE_productName___InteractorProtocol? {get set}
-    var view: ___VARIABLE_productName___ViewProtocol? {get set}
+    var router: ___VARIABLE_productName___RouterProtocol? { get set }
+    var interactor: ___VARIABLE_productName___InteractorProtocol? { get set }
+    var view: ___VARIABLE_productName___ViewProtocol? { get set }
     
     // View -> Presenter
     // Interactor -> Presenter
